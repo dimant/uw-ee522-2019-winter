c8caeecf5d72940bf746477881e9210b4239cef3
Diman Todorov <dimant@uw.edu>
2019-02-11 04:45:09 +0000

    created pew

--- assignment3/sfx.c
+++ assignment3/sfx.c
@@ -16,7 +16,7 @@ uint32_t sfx_get_period(effect_t* effect, float* buffer, uint32_t samples)
         n = effect->size - effect->cursor;
     }
 
-    memcpy(buffer, effect->buffer + effect->cursor, n);
+    memcpy(buffer, effect->buffer + effect->cursor, sizeof(float) * n);
 
     effect->cursor += n;
     return TRUE;
@@ -37,22 +37,30 @@ void sfx_create_pew(effect_t* effect)
     ASSERT(effect != NULL);
     ASSERT(effect->buffer != NULL);
 
-    const uint32_t duration = 500; // ms
+    const uint32_t duration = 250; // ms
     const uint32_t samples = duration * effect->sampling_rate / 1000;
     ASSERT(samples < effect->sampling_rate);
     
-    const uint32_t freq_saw = 880;
+    const uint32_t freq_saw = 4;
     const uint32_t period_saw = effect->sampling_rate / freq_saw;
 
-    const uint32_t freq_sin = 440;
+    const uint32_t freq_sin = 554;
     const uint32_t period_sin = effect->sampling_rate / freq_sin;
     
     float buffer[effect->sampling_rate];
-    float angle = (float)freq_sin / (float)effect->sampling_rate;
 
-    audio_sin(buffer, samples, angle, period_sin, 0);
+    audio_noise(effect->buffer, samples);
 
-    audio_saw(effect->buffer, samples, period_saw, 0);
+    audio_saw(buffer, samples, period_sin, 0);
+
+    audio_add(effect->buffer, buffer, samples, 0.1f);
+
+    audio_saw(buffer, samples, period_saw, 0);
+
+    for(uint32_t s = 0; s < samples; s++)
+    {
+        buffer[s] = 1.0f - buffer[s];
+    }
 
     audio_mul(effect->buffer, buffer, samples);
 
